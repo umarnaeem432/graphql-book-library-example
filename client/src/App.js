f9f579fb2316d0dfa595d00a9d7f470663d3a396
M.Umar <umarnaeem432@gmail.com>
2020-03-01 20:33:41 +0500

    umarnaeem432- React Setup- Added React Component folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import BookList from './components/BookList';
+
+class App extends Component {
+  render () {
+    return (
+      <div id="main">
+        <h1>Reading List</h1>
+        <BookList />
+      </div>
+    );
+  }
 }
 
 export default App;
